b8c4ae52890a60a42e6ad40d1fc55cd5f2218776
Zhaopudark <zhaopudark@outlook.com>
2024-01-12 12:10:07 +0800

    Init v0.0.1

--- helper/local_test.ps1
+++ helper/local_test.ps1
@@ -3,4 +3,4 @@ pip install -U pytest pytest-cov
 $root_path = (Get-Item "$PSScriptRoot/..").FullName
 $Env:PYTHONPATH="$root_path/src"
 Write-Host "Python path:" $Env:PYTHONPATH
-pytest "$root_path\tests\"  --log-level=INFO --cov $Env:PYTHONPATH --cov-report=xml
+pytest "$root_path\tests"  --log-level=INFO --cov $Env:PYTHONPATH --cov-report=xml
