943b74d059519b59eb2dc43aa5518c565626a7a8
Zhaopudark <zhaopudark@outlook.com>
2024-01-18 18:49:49 +0800

    Prepare for publish

--- scripts/tag_and_push.ps1
+++ scripts/tag_and_push.ps1
@@ -1,29 +1,26 @@
 $version = . "${PSScriptRoot}/check_release_version.ps1"
-# Write-Host 
-
 $tagName = "v${version}"
+
 # 检测标签是否存在
-if (git show-ref --quiet "refs/tags/$tagName") {
-    # 标签存在，删除它
-    git tag -d $tagName
-    Write-Output "Tag '$tagName' deleted."
+if (git show-ref "refs/tags/$tagName") {
+    # 标签存在，强制覆盖
+    Write-Output "Covering tag '$tagName' forcibly."
+    git tag -a $tagName -m "Release $tagName" --force
 } else {
     # 标签不存在
     Write-Output "Tag '$tagName' does not exist."
     Write-Output "Creating tag '$tagName'."
+    git tag -a $tagName -m "Release $tagName"
 }
-git tag -a $tagName -m "Release $tagName"
 
 # 检测远程标签是否存在
-$remoteTagExists = git ls-remote --tags origin | Where-Object { $_ -match "refs/tags/$tagName" }
-
-if ($remoteTagExists) {
-    # 远程标签存在，删除它
-    git push origin --delete $tagName
-    Write-Output "Remote tag '$tagName' deleted."
+if (git ls-remote --tags origin | Where-Object { $_ -match "refs/tags/$tagName" }) {
+    # 远程标签不存在
+    Write-Output "Covering remote tag '$tagName' forcibly."
+    git push origin $tagName --force
 } else {
     # 远程标签不存在
     Write-Output "Remote tag '$tagName' does not exist."
     Write-Output "Pushing tag '$tagName' to remote."
+    git push origin $tagName
 }
-git push origin $tagName
