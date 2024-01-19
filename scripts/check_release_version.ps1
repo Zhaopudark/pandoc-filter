14a97ef0ea860a4936c4813ea6e9b077ad374dc7
Zhaopudark <zhaopudark@outlook.com>
2024-01-19 17:30:38 +0800

    Prepare for publish

--- scripts/check_release_version.ps1
+++ scripts/check_release_version.ps1
@@ -4,7 +4,7 @@ $root_path = (Get-Item "$PSScriptRoot/..").FullName
 $src_path = "${root_path}/src"
 $release_note_path = "${root_path}/RELEASE.md"
 $version = python "${PSScriptRoot}/check_release_version.py" $src_path $release_note_path
-Write-Host $version
+# Write-Host $version
 if ($LastExitCode -ne 0) {
     throw "The release version in ${release_note_path} is not consistent with the given version in ${root_path}/src/pandoc_filter/__init__.py."
 }
