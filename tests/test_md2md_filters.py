731de18db296d4243e8cb9a495704e9ad93d8dbc
Zhaopudark <zhaopudark@outlook.com>
2024-01-21 03:41:45 +0800

    change APIs

--- tests/test_md2md_filters.py
+++ tests/test_md2md_filters.py
@@ -20,87 +20,87 @@ def _compare_files(file1_path, file2_path):
     else:
         return False
 
-# def test_md2md_footnote_filter():
-#     file_path = _check_file_path("./resources/inputs/test_md2md_footnote.md")
-#     pathlib.Path("./temp").mkdir(parents=True, exist_ok=True)
-#     output_path = pathlib.Path(f"./temp/{file_path.name}")
-#     answer_path = pathlib.Path(f"./resources/outputs/{file_path.name}")
-#     pandoc_command = [
-#         'pandoc',
-#         file_path,
-#         '-o',
-#         output_path,
-#         '-f',
-#         'markdown',
-#         '-t',
-#         'gfm',
-#         '-s',
-#         '--filter',
-#         'md2md-footnote'
-#     ]
-#     assert subprocess.run(pandoc_command, check=True).returncode == 0
-#     assert _compare_files(output_path,answer_path)
+def test_md2md_footnote_filter():
+    file_path = _check_file_path("./resources/inputs/test_md2md_footnote.md")
+    pathlib.Path("./temp").mkdir(parents=True, exist_ok=True)
+    output_path = pathlib.Path(f"./temp/{file_path.name}")
+    answer_path = pathlib.Path(f"./resources/outputs/{file_path.name}")
+    pandoc_command = [
+        'pandoc',
+        file_path,
+        '-o',
+        output_path,
+        '-f',
+        'markdown',
+        '-t',
+        'gfm',
+        '-s',
+        '--filter',
+        'md2md-footnote'
+    ]
+    assert subprocess.run(pandoc_command, check=True).returncode == 0
+    assert _compare_files(output_path,answer_path)
 
-# def test_md2md_internal_link_filter():
-#     file_path = _check_file_path("./resources/inputs/test_md2md_internal_link.md")
-#     pathlib.Path("./temp").mkdir(parents=True, exist_ok=True)
-#     output_path = pathlib.Path(f"./temp/{file_path.name}")
-#     answer_path = pathlib.Path(f"./resources/outputs/{file_path.name}")
-#     pandoc_command = [
-#         'pandoc',
-#         file_path,
-#         '-o',
-#         output_path,
-#         '-f',
-#         'markdown',
-#         '-t',
-#         'gfm',
-#         '-s',
-#         '--filter',
-#         'md2md-link'
-#     ]
-#     assert subprocess.run(pandoc_command, check=True).returncode == 0
-#     assert _compare_files(output_path,answer_path)
+def test_md2md_internal_link_filter():
+    file_path = _check_file_path("./resources/inputs/test_md2md_internal_link.md")
+    pathlib.Path("./temp").mkdir(parents=True, exist_ok=True)
+    output_path = pathlib.Path(f"./temp/{file_path.name}")
+    answer_path = pathlib.Path(f"./resources/outputs/{file_path.name}")
+    pandoc_command = [
+        'pandoc',
+        file_path,
+        '-o',
+        output_path,
+        '-f',
+        'markdown',
+        '-t',
+        'gfm',
+        '-s',
+        '--filter',
+        'md2md-link'
+    ]
+    assert subprocess.run(pandoc_command, check=True).returncode == 0
+    assert _compare_files(output_path,answer_path)
 
 
-# def test_md2md_math_filter():
-#     file_path = _check_file_path("./resources/inputs/test_md2md_math.md")
-#     pathlib.Path("./temp").mkdir(parents=True, exist_ok=True)
-#     output_path = pathlib.Path(f"./temp/{file_path.name}")
-#     answer_path = pathlib.Path(f"./resources/outputs/{file_path.name}")
-#     pandoc_command = [
-#         'pandoc',
-#         file_path,
-#         '-o',
-#         output_path,
-#         '-f',
-#         'markdown',
-#         '-t',
-#         'gfm',
-#         '-s',
-#         '--filter',
-#         'md2md-math'
-#     ]
-#     assert subprocess.run(pandoc_command, check=True).returncode == 0
-#     assert _compare_files(output_path,answer_path)
+def test_md2md_math_filter():
+    file_path = _check_file_path("./resources/inputs/test_md2md_math.md")
+    pathlib.Path("./temp").mkdir(parents=True, exist_ok=True)
+    output_path = pathlib.Path(f"./temp/{file_path.name}")
+    answer_path = pathlib.Path(f"./resources/outputs/{file_path.name}")
+    pandoc_command = [
+        'pandoc',
+        file_path,
+        '-o',
+        output_path,
+        '-f',
+        'markdown',
+        '-t',
+        'gfm',
+        '-s',
+        '--filter',
+        'md2md-math'
+    ]
+    assert subprocess.run(pandoc_command, check=True).returncode == 0
+    assert _compare_files(output_path,answer_path)
 
-# def test_md2md_figure_filter():
-#     file_path = _check_file_path("./resources/inputs/test_md2md_figure.md")
-#     pathlib.Path("./temp").mkdir(parents=True, exist_ok=True)
-#     output_path = pathlib.Path(f"./temp/{file_path.name}")
-#     answer_path = pathlib.Path(f"./resources/outputs/{file_path.name}")
-#     pandoc_command = [
-#         'pandoc',
-#         file_path,
-#         '-o',
-#         output_path,
-#         '-f',
-#         'markdown',
-#         '-t',
-#         'gfm',
-#         '-s',
-#         '--filter',
-#         'md2md-figure'
-#     ]
-#     assert subprocess.run(pandoc_command, check=True,env={'CURRENT_DOC_PATH':str(file_path), **subprocess.os.environ}).returncode == 0
-#     assert _compare_files(output_path,answer_path)
+def test_md2md_figure_filter():
+    file_path = _check_file_path("./resources/inputs/test_md2md_figure.md")
+    pathlib.Path("./temp").mkdir(parents=True, exist_ok=True)
+    output_path = pathlib.Path(f"./temp/{file_path.name}")
+    answer_path = pathlib.Path(f"./resources/outputs/{file_path.name}")
+    pandoc_command = [
+        'pandoc',
+        file_path,
+        '-o',
+        output_path,
+        '-f',
+        'markdown',
+        '-t',
+        'gfm',
+        '-s',
+        '--filter',
+        'md2md-figure'
+    ]
+    assert subprocess.run(pandoc_command, check=True,env={'CURRENT_DOC_PATH':str(file_path), **subprocess.os.environ}).returncode == 0
+    assert _compare_files(output_path,answer_path)
