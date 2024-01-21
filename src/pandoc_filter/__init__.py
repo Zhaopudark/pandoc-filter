6bfe200a163e38847b23f535a86480a55f856e29
Zhaopudark <zhaopudark@outlook.com>
2024-01-21 20:25:40 +0800

    change APIs

--- src/pandoc_filter/__init__.py
+++ src/pandoc_filter/__init__.py
@@ -1,8 +1,12 @@
+"""
+See 
+"""
 
 from .scripts import (
     md2md_figure_filter,
     md2md_footnote_filter,
     md2md_internal_link_filter,
+    md2md_math_filter,
     md2html_anchor_and_internal_link_filter,
     md2html_figure_filter,
     md2html_link_like_filter)
