d4cdcc7bdc5cb8aba114bc3463e72938b65bf91d
Zhaopudark <zhaopudark@outlook.com>
2024-02-25 03:31:50 +0800

    parepare for v0.2.6

--- tests/test_md2md_filters.py
+++ tests/test_md2md_filters.py
@@ -119,16 +119,15 @@ def test_md2md_enhance_equation_filter_pyio():
     pandoc_filter.run_filters_pyio(file_path,output_path,'markdown','gfm',[pandoc_filter.md2md_enhance_equation_filter],finalize=finalize)
     assert _check_the_same_content(output_path,answer_path)
     
-# def test_md2md_upload_figure_to_aliyun_filter():
-#     file_path = _check_file_path("./resources/inputs/test_md2md_figure.md")
-#     pathlib.Path("./temp").mkdir(parents=True, exist_ok=True)
-#     output_path = pathlib.Path(f"./temp/{file_path.name}")
-#     answer_path = pathlib.Path(f"./resources/outputs/{file_path.name}")
-#     pandoc_filter.run_filters_pyio(
-#         file_path,output_path,'markdown','gfm',
-#         [pandoc_filter.md2md_upload_figure_to_aliyun_filter],doc_path=file_path)
-#     assert _check_the_same_content(output_path,answer_path)
-
+def test_md2md_upload_figure_to_aliyun_filter():
+    file_path = _check_file_path("./resources/inputs/test_md2md_figure.md")
+    pathlib.Path("./temp").mkdir(parents=True, exist_ok=True)
+    output_path = pathlib.Path(f"./temp/{file_path.name}")
+    answer_path = pathlib.Path(f"./resources/outputs/{file_path.name}")
+    pandoc_filter.run_filters_pyio(
+        file_path,output_path,'markdown','gfm',
+        [pandoc_filter.md2md_upload_figure_to_aliyun_filter],doc_path=file_path)
+    assert _check_the_same_content(output_path,answer_path)
 
 def test_md2md_convert_github_style_alert_to_hexo_style_alert_filter_pyio():
     file_path = _check_file_path("./resources/inputs/test_md2md_alert.md")
