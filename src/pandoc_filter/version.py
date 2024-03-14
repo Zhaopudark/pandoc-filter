75ac32b336e967269536e8cb811a5c661ca99e5c
Zhaopudark <zhaopudark@outlook.com>
2024-03-14 19:25:51 +0800

    Add `md2html_increase_header_level_filter`, which will increase the header level by 1.

--- src/pandoc_filter/version.py
+++ src/pandoc_filter/version.py
@@ -3,5 +3,5 @@ pandoc-filter version
 """
 from .utils import check_pandoc_version
 
-check_pandoc_version(required_version='3.1.12')
+check_pandoc_version(required_version='3.1.12.2')
 __version__ = '0.2.13'
