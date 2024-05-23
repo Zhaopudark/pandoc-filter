6ba940be70730a1fef856f582ca68328dff01590
Zhaopudark <zhaopudark@outlook.com>
2024-05-23 21:55:38 +0800

    Modify md2md-norm-footnote-filter.

--- resources/inputs/test_md2md_footnote.md
+++ resources/inputs/test_md2md_footnote.md
@@ -1,8 +1,8 @@
-which1.[^1]
+duplicate1.[^1]
 
-which2.[^2]
+duplicate2.[^2]
 
-which3.[^3]
+duplicate3.[^3]
 
 [^1]: Deep Learning with Intel® AVX-512 and Intel® DL Boost
 https://www.intel.cn/content/www/cn/zh/developer/articles/guide/deep-learning-with-avx512-and-dl-boost.html
@@ -11,3 +11,12 @@ www.intel.cn
 [^2]: Deep Learning with Intel® AVX-512222 and Intel® DL Boost https://www.intel.cn/content/www/cn/zh/developer/articles/guide/deep-learning-with-avx512-and-dl-boost.html www.intel.cn
 
 [^3]: Deep Learning with Intel®     AVX-512 and Intel® DL Boost https://www.intel.cn/content/www/cn/zh/developer/articles/guide/deep-learning-with-avx512-and-dl-boost.html www.intel.cn
+
+
+emphasis(italic).[^4]
+strong(bold).[^5]
+
+[^4]:《Sorting algorithm》, *Wikipedia*. 2024年3月19日. 见于: 2024年5月22日. [在线]. 载于: https://en.wikipedia.org/w/index.php?title=Sorting_algorithm&oldid=1214598553#Stability
+
+
+[^5]:《Sorting algorithm》, **Wikipedia**. 2024年3月19日. 见于: 2024年5月22日. [在线]. 载于: https://en.wikipedia.org/w/index.php?title=Sorting_algorithm&oldid=1214598553#Stability
--- src/pandoc_filter/filters/md2md/norm_footnote.py
+++ src/pandoc_filter/filters/md2md/norm_footnote.py
@@ -3,35 +3,25 @@ import panflute as pf
 from ...utils import TracingLogger
 
 r"""A pandoc filter that mainly for converting `markdown` to `markdown`.
-Normalize the footnotes. Remove unnecessary `\n` in the footnote content.
+Normalize the footnotes. Deal with the footnote content as follows:
+    - Remove unnecessary `\n`.
+    - Remove link.
+    - Normalize many formats like emphasis(italic) and strong(bold).
+
 """
-
-def __deserialize_link_to_markdown(link:pf.Link)->str:
-    return f"[{pf.stringify(link)}]({link.url})"
-
+        
 def _norm_footnote(elem:pf.Element,doc:pf.Doc,tracing_logger:TracingLogger,**kwargs)->pf.Note|None:
     r"""Follow the general procedure of [Panflute](http://scorreia.com/software/panflute/)
-    An action to process footnotes. Remove unnecessary `\n` in the footnote content.
-    
-    When converting `markdown` to `markdown`, if there is any pf.Space in pf.Note(pf.Para(...)), the 
-    panflute/pandoc will layout elements and add `\n` to the content with some unkonwn rules.
-    To remove the unnecessary `\n`, we need to aggregate words into a whole string, as this function does.
+    An action to process footnotes. Deal with the footnote content as follows:
+        - Remove unnecessary `\n`.
+        - Remove link.
+        - Normalize many formats like emphasis(italic) and strong(bold).
     
     [replace elements]
     """
     if isinstance(elem, pf.Note):
         tracing_logger.mark(elem)
-        new_string = ""
-        for para in elem.content:
-            assert isinstance(para,pf.Para)
-            for item in para.content:
-                if isinstance(item,pf.Space):
-                    new_string = new_string.strip(" \n") + " "
-                elif isinstance(item,pf.Str):
-                    new_string = new_string.strip(" \n") + " " + item.text.strip(" \n")
-                elif isinstance(item,pf.Link):
-                    new_string = new_string.strip(" \n") + " " + __deserialize_link_to_markdown(item).strip(" \n")
-        elem = pf.Note(pf.Para(pf.Str(new_string)))
+        elem = pf.Note(pf.Para(pf.Str(pf.stringify(elem))))
         tracing_logger.check_and_log('footnote',elem)
         return elem
 
